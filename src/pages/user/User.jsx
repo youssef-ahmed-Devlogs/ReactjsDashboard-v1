d4bc78c0c1b0aa3c1cba6b4d404dac9b47902356
youssefahmeddevlogs <youssefahmedsup@gmail.com>
2021-12-18 11:27:40 +0200

    updates

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,4 +1,12 @@
-import { PermIdentity } from "@material-ui/icons";
+import {
+  CalendarToday,
+  LocationSearching,
+  MailOutline,
+  PermIdentity,
+  PhoneAndroid,
+  Publish,
+} from "@material-ui/icons";
+import { Link } from "react-router-dom";
 import "./user.css";
 
 export default function User() {
@@ -6,7 +14,9 @@ export default function User() {
     <div className="user">
       <div className="userTitleContainer">
         <h1 className="userTitle">Edit User</h1>
-        <button className="userAddButton">Create</button>
+        <Link to="/newUser">
+          <button className="userAddButton">Create</button>
+        </Link>
       </div>
       <div className="userContainer">
         <div className="userShow">
@@ -28,24 +38,86 @@ export default function User() {
               <span className="userShowInfoTitle">annabeck99</span>
             </div>
             <div className="userShowInfo">
-              <PermIdentity className="userShowIcon" />
+              <CalendarToday className="userShowIcon" />
               <span className="userShowInfoTitle">10.12.1999</span>
             </div>
+
+            <span className="userShowTitle">Contact Details</span>
             <div className="userShowInfo">
-              <PermIdentity className="userShowIcon" />
+              <PhoneAndroid className="userShowIcon" />
               <span className="userShowInfoTitle">+1 123 456 78</span>
             </div>
             <div className="userShowInfo">
-              <PermIdentity className="userShowIcon" />
+              <MailOutline className="userShowIcon" />
               <span className="userShowInfoTitle">annabeck99@gmail.com</span>
             </div>
             <div className="userShowInfo">
-              <PermIdentity className="userShowIcon" />
+              <LocationSearching className="userShowIcon" />
               <span className="userShowInfoTitle">New York | USA</span>
             </div>
           </div>
         </div>
-        <div className="userUpdate"></div>
+        <div className="userUpdate">
+          <span className="userUpdateTitle">Edit</span>
+          <form className="userUpdateForm">
+            <div className="userUpdateLeft">
+              <div className="userUpdateItem">
+                <label>Username</label>
+                <input
+                  type="text"
+                  placeholder="annabeck99"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  placeholder="Anna Becker"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Email</label>
+                <input
+                  type="text"
+                  placeholder="annabeck99@gmail.com"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Phone</label>
+                <input
+                  type="text"
+                  placeholder="+1 123 456 78"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Address</label>
+                <input
+                  type="text"
+                  placeholder="New York | USA"
+                  className="userUpdateInput"
+                />
+              </div>
+            </div>
+            <div className="userUpdateRight">
+              <div className="userUpdateUpload">
+                <img
+                  className="userUpdateImg"
+                  src="https://i.picsum.photos/id/64/300/300.jpg?hmac=tIqPMmxrlIvlgHwBqR7lzPQyHO-KElXvhXwtKuyPse4"
+                  alt="User image"
+                />
+                <label htmlFor="file" className="uploadBtn" title="upload">
+                  <Publish className="uploadIcon" />
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+              <button className="userUpdateButton">Update</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
